90ae8b2dbd70dd5ba3dbbb2d7249895fdc9f4d48
Joshua Abdiel <joshuaabdiel365@gmail.com>
2026-09-05 19:51:35 +0700

    feat: add Sticky Scroll & Cards Stacking Parallax section based on Framer & Hello-Mat design engineering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/hero";
 import { Projects } from "@/components/projects";
+import { StackingCardsSection } from "@/components/stacking-cards";
 import { Dossier } from "@/components/dossier";
 import { Principles } from "@/components/principles";
 import { Workspace } from "@/components/workspace";
@@ -14,6 +15,7 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Projects />
+        <StackingCardsSection />
         <Dossier />
         <Principles />
         <Workspace />
